1da129bfc60e5bbb7de10067ef6a1d0c5a9902c2
juftin <juftin@juftin.com>
2024-02-19 22:22:50 -0700

    🎨 linting

--- textual_universal_directorytree/__init__.py
+++ textual_universal_directorytree/__init__.py
@@ -2,9 +2,11 @@
 Textual Universal Directory Tree
 """
 
-from .alternate_paths import GitHubPath, S3TextualPath
-from .universal_directory_tree import UniversalDirectoryTree
-from .utils import is_local_path, is_remote_path
+from textual_universal_directorytree.alternate_paths import GitHubPath, S3TextualPath
+from textual_universal_directorytree.universal_directory_tree import (
+    UniversalDirectoryTree,
+)
+from textual_universal_directorytree.utils import is_local_path, is_remote_path
 
 __all__ = [
     "UniversalDirectoryTree",
